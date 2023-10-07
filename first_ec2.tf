5b1ee533ded364c374869024430565fb0bdcd575
Aalim <Aalim.kota@gmail.com>
2023-10-07 23:18:55 +0530

    ec2 creation file

---
+++ first_ec2.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "ap-south-1"
+    access_key = "AKIAUWN3AVVB36SZQBO6"
+    secret_key = "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"
+}
+/*
+resource "aws_instance" "myec2" {
+  ami = "ami-0e6329e222e662a52"
+  instance_type = "t2.micro"
+}
+*/
